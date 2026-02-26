c597329bfdfa13ea466dcabd53e5b3dc561e14f2
Vivek <vivekmunde@users.noreply.github.com>
2026-02-26 17:01:36 +0530

    Detect request language

--- src/initialize-i18n.ts
+++ src/initialize-i18n.ts
@@ -1,16 +1,26 @@
 import i18next from "i18next";
 import i18nextFsBackend from "i18next-fs-backend";
+import { LanguageDetector } from "i18next-http-middleware";
 import path from "path";
 
 const initializeI18n = () => {
-  i18next.use(i18nextFsBackend).init({
-    fallbackLng: "en",
-    preload: ["en", "nl"],
-    ns: ["error-codes", "validations", "emails"],
-    backend: {
-      loadPath: path.join(__dirname, "..", "locales", "{{lng}}", "{{ns}}.json"),
-    },
-  });
+  i18next
+    .use(i18nextFsBackend)
+    .use(LanguageDetector)
+    .init({
+      fallbackLng: "en",
+      preload: ["en"],
+      ns: ["word"],
+      backend: {
+        loadPath: path.join(__dirname, "locales", "{{lng}}", "{{ns}}.json"),
+      },
+      detection: {
+        order: ["querystring", "cookie", "header"],
+        lookupQuerystring: "lng",
+        lookupCookie: "i18next",
+        caches: ["cookie"],
+      },
+    });
 
   return i18next;
 };
